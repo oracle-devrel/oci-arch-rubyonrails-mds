5450912b0dec0bbb6adc4159bb71ed859a2030a1
Phil Wilkins <phil@mp3monster.org>
2022-08-02 12:29:04 +0100

    move vtags over to using the generic module

--- common.tf
+++ common.tf
@@ -4,13 +4,13 @@
 locals {
   defined_tags = module.policies.predefined_tags
   # declare a local for defined tags to conceal from the rest of the module how we're creating them
-
   random_id = module.policies.random_id
+  # conceal from the rest of the module how this value is setup
 }
 module "policies" {
   #source = "../terraform-oci-arch-policies"
   source                        = "github.com/oracle-devrel/terraform-oci-arch-policies"
-  activate_policies_for_service = ["API"]
+  activate_policies_for_service = ["Network"]
   tenancy_ocid                  = var.tenancy_ocid
   compartment_ocid              = var.compartment_ocid
   region_name                   = var.region
